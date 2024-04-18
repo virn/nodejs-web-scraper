a7bf0204a18c531b6abc6b9ca5ce31c7a10ce173
Virn <virn.abuda@musictribe.com>
2024-04-18 14:12:43 +0800

    add todo

---
+++ todo.js
@@ -0,0 +1,9 @@
+/*
+1. Create a UI for word input
+2. Connect with BE
+3. Format Response
+4. Allow extract to excel
+5. Additional UI for Website source to be scrapped
+6. Additional parameters for query
+7. Format respon
+*/
